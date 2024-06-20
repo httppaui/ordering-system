3eb99e9953d5a42570cd633ad4012880d4736fa0
paui <163615470+httppaui@users.noreply.github.com>
2024-06-20 10:06:30 +0800

    Add files via upload

---
+++ 169_Pamittan_PaulaHewlett_Pasicolan_Andrei/169_Pamittan_PaulaHewlett_Pasicolan_Andrei/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace _169_Pamittan_PaulaHewlett_Pasicolan_Andrei
+{
+    partial class frmScarbats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nuD1 = new System.Windows.Forms.NumericUpDown();
+            this.chkCreamyCaramel = new System.Windows.Forms.CheckBox();
+            this.chkSaltedCaramel = new System.Windows.Forms.CheckBox();
+            this.nuD2 = new System.Windows.Forms.NumericUpDown();
+            this.chkVanilla = new System.Windows.Forms.CheckBox();
+            this.nuD3 = new System.Windows.Forms.NumericUpDown();
+            this.chkBurnt = new System.Windows.Forms.CheckBox();
+            this.nuD4 = new System.Windows.Forms.NumericUpDown();
+            this.chkHotBrewed = new System.Windows.Forms.CheckBox();
+            this.nuD5 = new System.Windows.Forms.NumericUpDown();
+            this.chkIcedCaramel = new System.Windows.Forms.CheckBox();
+            this.nuD6 = new System.Windows.Forms.NumericUpDown();
+            this.chkGreenTea = new System.Windows.Forms.CheckBox();
+            this.nuD7 = new System.Windows.Forms.NumericUpDown();
+            this.chkJava = new System.Windows.Forms.CheckBox();
+            this.nuD8 = new System.Windows.Forms.NumericUpDown();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdToGo = new System.Windows.Forms.RadioButton();
+            this.rdHere = new System.Windows.Forms.RadioButton();
+            this.chkMember = new System.Windows.Forms.CheckBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD8)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scarbats Coffee Ordering System";
+            // 
+            // nuD1
+            // 
+            this.nuD1.Enabled = false;
+            this.nuD1.Location = new System.Drawing.Point(44, 76);
+            this.nuD1.Name = "nuD1";
+            this.nuD1.Size = new System.Drawing.Size(37, 20);
+            this.nuD1.TabIndex = 1;
+            this.nuD1.ValueChanged += new System.EventHandler(this.nuD1_ValueChanged);
+            // 
+            // chkCreamyCaramel
+            // 
+            this.chkCreamyCaramel.AutoSize = true;
+            this.chkCreamyCaramel.Location = new System.Drawing.Point(87, 78);
+            this.chkCreamyCaramel.Name = "chkCreamyCaramel";
+            this.chkCreamyCaramel.Size = new System.Drawing.Size(129, 17);
+            this.chkCreamyCaramel.TabIndex = 2;
+            this.chkCreamyCaramel.Text = "Creamy Caramel Latte";
+            this.chkCreamyCaramel.UseVisualStyleBackColor = true;
+            this.chkCreamyCaramel.CheckedChanged += new System.EventHandler(this.chkCreamyCaramel_CheckedChanged);
+            // 
+            // chkSaltedCaramel
+            // 
+            this.chkSaltedCaramel.AutoSize = true;
+            this.chkSaltedCaramel.Location = new System.Drawing.Point(87, 114);
+            this.chkSaltedCaramel.Name = "chkSaltedCaramel";
+            this.chkSaltedCaramel.Size = new System.Drawing.Size(124, 17);
+            this.chkSaltedCaramel.TabIndex = 4;
+            this.chkSaltedCaramel.Text = "Salted Caramel Latte";
+            this.chkSaltedCaramel.UseVisualStyleBackColor = true;
+            this.chkSaltedCaramel.CheckedChanged += new System.EventHandler(this.chkSaltedCaramel_CheckedChanged);
+            // 
+            // nuD2
+            // 
+            this.nuD2.Enabled = false;
+            this.nuD2.Location = new System.Drawing.Point(44, 112);
+            this.nuD2.Name = "nuD2";
+            this.nuD2.Size = new System.Drawing.Size(37, 20);
+            this.nuD2.TabIndex = 3;
+            // 
+            // chkVanilla
+            // 
+            this.chkVanilla.AutoSize = true;
+            this.chkVanilla.Location = new System.Drawing.Point(87, 150);
+            this.chkVanilla.Name = "chkVanilla";
+            this.chkVanilla.Size = new System.Drawing.Size(174, 17);
+            this.chkVanilla.TabIndex = 6;
+            this.chkVanilla.Text = "Vanilla Sweet Cream Cold Brew";
+            this.chkVanilla.UseVisualStyleBackColor = true;
+            this.chkVanilla.CheckedChanged += new System.EventHandler(this.chkVanilla_CheckedChanged);
+            // 
+            // nuD3
+            // 
+            this.nuD3.Enabled = false;
+            this.nuD3.Location = new System.Drawing.Point(44, 148);
+            this.nuD3.Name = "nuD3";
+            this.nuD3.Size = new System.Drawing.Size(37, 20);
+            this.nuD3.TabIndex = 5;
+            // 
+            // chkBurnt
+            // 
+            this.chkBurnt.AutoSize = true;
+            this.chkBurnt.Location = new System.Drawing.Point(87, 184);
+            this.chkBurnt.Name = "chkBurnt";
+            this.chkBurnt.Size = new System.Drawing.Size(157, 17);
+            this.chkBurnt.TabIndex = 8;
+            this.chkBurnt.Text = "Burnt Caramel Oatmilk Latte";
+            this.chkBurnt.UseVisualStyleBackColor = true;
+            this.chkBurnt.CheckedChanged += new System.EventHandler(this.chkBurnt_CheckedChanged);
+            // 
+            // nuD4
+            // 
+            this.nuD4.Enabled = false;
+            this.nuD4.Location = new System.Drawing.Point(44, 182);
+            this.nuD4.Name = "nuD4";
+            this.nuD4.Size = new System.Drawing.Size(37, 20);
+            this.nuD4.TabIndex = 7;
+            // 
+            // chkHotBrewed
+            // 
+            this.chkHotBrewed.AutoSize = true;
+            this.chkHotBrewed.Location = new System.Drawing.Point(323, 79);
+            this.chkHotBrewed.Name = "chkHotBrewed";
+            this.chkHotBrewed.Size = new System.Drawing.Size(116, 17);
+            this.chkHotBrewed.TabIndex = 10;
+            this.chkHotBrewed.Text = "Hot Brewed Coffee";
+            this.chkHotBrewed.UseVisualStyleBackColor = true;
+            this.chkHotBrewed.CheckedChanged += new System.EventHandler(this.chkHotBrewed_CheckedChanged);
+            // 
+            // nuD5
+            // 
+            this.nuD5.Enabled = false;
+            this.nuD5.Location = new System.Drawing.Point(280, 77);
+            this.nuD5.Name = "nuD5";
+            this.nuD5.Size = new System.Drawing.Size(37, 20);
+            this.nuD5.TabIndex = 9;
+            // 
+            // chkIcedCaramel
+            // 
+            this.chkIcedCaramel.AutoSize = true;
+            this.chkIcedCaramel.Location = new System.Drawing.Point(323, 115);
+            this.chkIcedCaramel.Name = "chkIcedCaramel";
+            this.chkIcedCaramel.Size = new System.Drawing.Size(141, 17);
+            this.chkIcedCaramel.TabIndex = 12;
+            this.chkIcedCaramel.Text = "Iced Caramel Macchiato";
+            this.chkIcedCaramel.UseVisualStyleBackColor = true;
+            this.chkIcedCaramel.CheckedChanged += new System.EventHandler(this.chkIcedCaramel_CheckedChanged);
+            // 
+            // nuD6
+            // 
+            this.nuD6.Enabled = false;
+            this.nuD6.Location = new System.Drawing.Point(280, 113);
+            this.nuD6.Name = "nuD6";
+            this.nuD6.Size = new System.Drawing.Size(37, 20);
+            this.nuD6.TabIndex = 11;
+            // 
+            // chkGreenTea
+            // 
+            this.chkGreenTea.AutoSize = true;
+            this.chkGreenTea.Location = new System.Drawing.Point(323, 151);
+            this.chkGreenTea.Name = "chkGreenTea";
+            this.chkGreenTea.Size = new System.Drawing.Size(166, 17);
+            this.chkGreenTea.TabIndex = 14;
+            this.chkGreenTea.Text = "Green Tea Cream Frappucino";
+            this.chkGreenTea.UseVisualStyleBackColor = true;
+            this.chkGreenTea.CheckedChanged += new System.EventHandler(this.chkGreenTea_CheckedChanged);
+            // 
+            // nuD7
+            // 
+            this.nuD7.Enabled = false;
+            this.nuD7.Location = new System.Drawing.Point(280, 149);
+            this.nuD7.Name = "nuD7";
+            this.nuD7.Size = new System.Drawing.Size(37, 20);
+            this.nuD7.TabIndex = 13;
+            // 
+            // chkJava
+            // 
+            this.chkJava.AutoSize = true;
+            this.chkJava.Location = new System.Drawing.Point(323, 186);
+            this.chkJava.Name = "chkJava";
+            this.chkJava.Size = new System.Drawing.Size(132, 17);
+            this.chkJava.TabIndex = 16;
+            this.chkJava.Text = "Java Chip Frappucino ";
+            this.chkJava.UseVisualStyleBackColor = true;
+            this.chkJava.CheckedChanged += new System.EventHandler(this.chkJava_CheckedChanged);
+            // 
+            // nuD8
+            // 
+            this.nuD8.Enabled = false;
+            this.nuD8.Location = new System.Drawing.Point(280, 184);
+            this.nuD8.Name = "nuD8";
+            this.nuD8.Size = new System.Drawing.Size(37, 20);
+            this.nuD8.TabIndex = 15;
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Location = new System.Drawing.Point(276, 245);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(110, 23);
+            this.btnComplete.TabIndex = 17;
+            this.btnComplete.Text = "Complete Order";
+            this.btnComplete.UseVisualStyleBackColor = true;
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(408, 245);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdToGo);
+            this.groupBox1.Controls.Add(this.rdHere);
+            this.groupBox1.Controls.Add(this.chkMember);
+            this.groupBox1.Location = new System.Drawing.Point(49, 225);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 125);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Membership and Dining";
+            // 
+            // rdToGo
+            // 
+            this.rdToGo.AutoSize = true;
+            this.rdToGo.Location = new System.Drawing.Point(29, 84);
+            this.rdToGo.Name = "rdToGo";
+            this.rdToGo.Size = new System.Drawing.Size(53, 17);
+            this.rdToGo.TabIndex = 2;
+            this.rdToGo.TabStop = true;
+            this.rdToGo.Text = "To go";
+            this.rdToGo.UseVisualStyleBackColor = true;
+            this.rdToGo.CheckedChanged += new System.EventHandler(this.rdToGo_CheckedChanged);
+            // 
+            // rdHere
+            // 
+            this.rdHere.AutoSize = true;
+            this.rdHere.Location = new System.Drawing.Point(29, 61);
+            this.rdHere.Name = "rdHere";
+            this.rdHere.Size = new System.Drawing.Size(66, 17);
+            this.rdHere.TabIndex = 1;
+            this.rdHere.TabStop = true;
+            this.rdHere.Text = "For Here";
+            this.rdHere.UseVisualStyleBackColor = true;
+            this.rdHere.CheckedChanged += new System.EventHandler(this.rdHere_CheckedChanged);
+            // 
+            // chkMember
+            // 
+            this.chkMember.AutoSize = true;
+            this.chkMember.Location = new System.Drawing.Point(29, 32);
+            this.chkMember.Name = "chkMember";
+            this.chkMember.Size = new System.Drawing.Size(83, 17);
+            this.chkMember.TabIndex = 0;
+            this.chkMember.Text = "Membership";
+            this.chkMember.UseVisualStyleBackColor = true;
+            this.chkMember.CheckedChanged += new System.EventHandler(this.chkMember_CheckedChanged);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(292, 305);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(45, 16);
+            this.lblTotal.TabIndex = 20;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoints.Location = new System.Drawing.Point(292, 334);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(51, 16);
+            this.lblPoints.TabIndex = 21;
+            this.lblPoints.Text = "Points: ";
+            // 
+            // frmScarbats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(532, 380);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.chkJava);
+            this.Controls.Add(this.nuD8);
+            this.Controls.Add(this.chkGreenTea);
+            this.Controls.Add(this.nuD7);
+            this.Controls.Add(this.chkIcedCaramel);
+            this.Controls.Add(this.nuD6);
+            this.Controls.Add(this.chkHotBrewed);
+            this.Controls.Add(this.nuD5);
+            this.Controls.Add(this.chkBurnt);
+            this.Controls.Add(this.nuD4);
+            this.Controls.Add(this.chkVanilla);
+            this.Controls.Add(this.nuD3);
+            this.Controls.Add(this.chkSaltedCaramel);
+            this.Controls.Add(this.nuD2);
+            this.Controls.Add(this.chkCreamyCaramel);
+            this.Controls.Add(this.nuD1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmScarbats";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scarbats Coffee";
+            this.Load += new System.EventHandler(this.frmScarbats_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nuD1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuD8)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nuD1;
+        private System.Windows.Forms.CheckBox chkCreamyCaramel;
+        private System.Windows.Forms.CheckBox chkSaltedCaramel;
+        private System.Windows.Forms.NumericUpDown nuD2;
+        private System.Windows.Forms.CheckBox chkVanilla;
+        private System.Windows.Forms.NumericUpDown nuD3;
+        private System.Windows.Forms.CheckBox chkBurnt;
+        private System.Windows.Forms.NumericUpDown nuD4;
+        private System.Windows.Forms.CheckBox chkHotBrewed;
+        private System.Windows.Forms.NumericUpDown nuD5;
+        private System.Windows.Forms.CheckBox chkIcedCaramel;
+        private System.Windows.Forms.NumericUpDown nuD6;
+        private System.Windows.Forms.CheckBox chkGreenTea;
+        private System.Windows.Forms.NumericUpDown nuD7;
+        private System.Windows.Forms.CheckBox chkJava;
+        private System.Windows.Forms.NumericUpDown nuD8;
+        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.Button btnReset;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdToGo;
+        private System.Windows.Forms.RadioButton rdHere;
+        private System.Windows.Forms.CheckBox chkMember;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblPoints;
+    }
+}
+
---
+++ 169_Pamittan_PaulaHewlett_Pasicolan_Andrei/169_Pamittan_PaulaHewlett_Pasicolan_Andrei/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _169_Pamittan_PaulaHewlett_Pasicolan_Andrei
+{
+    public partial class frmScarbats : Form
+    {
+        int item1 = 120, item2 = 110, item3 = 125, item4 = 150, item5 = 90, item6 = 135, item7 = 155, item8 = 162;
+
+        private void chkCreamyCaramel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkCreamyCaramel.Checked)
+            {
+                nuD1.Enabled = false;
+                nuD1.Value = 0;
+            }
+            else
+            {
+                nuD1.Enabled = true;
+                nuD1.Value = 1;
+            }
+        }
+
+        private void chkSaltedCaramel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkSaltedCaramel.Checked)
+            {
+                nuD2.Enabled = false;
+                nuD2.Value = 0;
+            }
+            else
+            {
+                nuD2.Enabled = true;
+                nuD2.Value = 1;
+            }
+        }
+
+        private void chkVanilla_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkVanilla.Checked)
+            {
+                nuD3.Enabled = false;
+                nuD3.Value = 0;
+            }
+            else
+            {
+                nuD3.Enabled = true;
+                nuD3.Value = 1;
+            }
+        }
+
+        private void chkBurnt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkBurnt.Checked)
+            {
+                nuD4.Enabled = false;
+                nuD4.Value = 0;
+            }
+            else
+            {
+                nuD4.Enabled = true;
+                nuD4.Value = 1;
+            }
+        }
+
+        private void chkHotBrewed_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkHotBrewed.Checked)
+            {
+                nuD5.Enabled = false;
+                nuD5.Value = 0;
+            }
+            else
+            {
+                nuD5.Enabled = true;
+                nuD5.Value = 1;
+            }
+        }
+
+        private void chkIcedCaramel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkIcedCaramel.Checked)
+            {
+                nuD6.Enabled = false;
+                nuD6.Value = 0;
+            }
+            else
+            {
+                nuD6.Enabled = true;
+                nuD6.Value = 1;
+            }
+        }
+
+        private void chkGreenTea_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkGreenTea.Checked)
+            {
+                nuD7.Enabled = false;
+                nuD7.Value = 0;
+            }
+            else
+            {
+                nuD7.Enabled = true;
+                nuD7.Value = 1;
+            }
+        }
+
+        private void chkJava_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkJava.Checked)
+            {
+                nuD8.Enabled = false;
+                nuD8.Value = 0;
+            }
+            else
+            {
+                nuD8.Enabled = true;
+                nuD8.Value = 1;
+            }
+        }
+
+        private void chkMember_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdHere_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdToGo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Uncheck all checkboxes
+            chkCreamyCaramel.Checked = false;
+            chkSaltedCaramel.Checked = false;
+            chkVanilla.Checked = false;
+            chkBurnt.Checked = false;
+            chkHotBrewed.Checked = false;
+            chkIcedCaramel.Checked = false;
+            chkGreenTea.Checked = false;
+            chkJava.Checked = false;
+            chkMember.Checked = false;
+            rdHere.Checked = false;
+            rdToGo.Checked = false;
+
+            // Reset numeric up-down controls to 0
+            nuD1.Value = 0;
+            nuD2.Value = 0;
+            nuD3.Value = 0;
+            nuD4.Value = 0;
+            nuD5.Value = 0;
+            nuD6.Value = 0;
+            nuD7.Value = 0;
+            nuD8.Value = 0;
+
+            // Clear total and points labels
+            lblTotal.Text = "Total: ";
+            lblPoints.Text = "Points: ";
+        }
+
+        private void nuD1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int discount = 50;
+        int addFee = 10;
+        double pts;
+
+        public frmScarbats()
+        {
+            InitializeComponent();
+        }
+
+        private void frmScarbats_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            double pts = 0.03;
+            if (chkCreamyCaramel.Checked)
+            {
+                total += item1 * (int)nuD1.Value;
+            }
+            if (chkSaltedCaramel.Checked)
+            {
+                total += item2 * (int)nuD2.Value;
+            }
+            if (chkVanilla.Checked)
+            {
+                total += item3 * (int)nuD3.Value;
+            }
+            if (chkBurnt.Checked)
+            {
+                total += item4 * (int)nuD4.Value;
+            }
+            if (chkHotBrewed.Checked)
+            {
+                total += item5 * (int)nuD5.Value;
+            }
+            if (chkIcedCaramel.Checked)
+            {
+                total += item6 * (int)nuD6.Value;
+            }
+            if (chkGreenTea.Checked)
+            {
+                total += item7 * (int)nuD7.Value;
+            }
+            if (chkJava.Checked)
+            {
+                total += item8 * (int)nuD8.Value;
+            }
+            // You can also add the discount and additional fee here
+
+            if (chkMember.Checked)
+            {
+                if (rdHere.Checked)
+                {
+                    total -= discount;
+                }
+                else
+                {
+                    total -= discount;
+                    total += addFee;
+                }
+            }
+            else
+            {
+                if (rdToGo.Checked)
+                {
+                    total += addFee;
+                }
+            }
+
+
+
+
+            // Update the total price label or textbox
+            if (!chkCreamyCaramel.Checked && !chkSaltedCaramel.Checked && !chkVanilla.Checked && !chkBurnt.Checked && !chkHotBrewed.Checked && !chkIcedCaramel.Checked && !chkGreenTea.Checked && !chkJava.Checked)
+            {
+                MessageBox.Show("Please select an item from the menu.", "Select item.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (chkMember.Checked)
+                {
+                    if (rdHere.Checked || rdToGo.Checked)
+                    {
+                        lblTotal.Text = "Total: " + total.ToString();
+                        lblPoints.Text = "Points: " + (total * pts).ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select dining method.", "Select dining method.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+                else
+                {
+
+                    if (rdHere.Checked || rdToGo.Checked)
+                    {
+                        lblTotal.Text = "Total: " + total.ToString();
+                        lblPoints.Text = "Points: 0";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select dining method.", "Select dining method.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+
+
+               }
+            }
+        }
